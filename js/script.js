260506757d87e59543d22a0879a3e3218087f832
Valentyn Lyaskovskiy <riodemouro2017@gmail.com>
2023-02-14 13:15:27 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const items = document.querySelectorAll(".item");
-for (i of items) {
+for (let i of items) {
   i.addEventListener("click", function () {
     if (this.classList.contains("active")) {
       this.classList.remove("active");
     } else {
-      for (i of items) {
+      for (let i of items) {
         i.classList.remove("active");
       }
       this.classList.add("active");
